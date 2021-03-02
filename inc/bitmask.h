074d842bbf61d58e098b4a7fab0219e4df7eb773
99003567 <78537995+99003567@users.noreply.github.com>
2021-03-02 13:59:04 +0530

    Add files via upload

---
+++ inc/bitmask.h
@@ -0,0 +1,7 @@
+#ifndef __BITMASK_H__
+#define __BITMASK_H__
+int set(int n, int k);
+int clear(int n, int k);
+int flip(int n, int k);
+int query(int n, int k);
+#endif
---
+++ inc/myutils.h
@@ -0,0 +1,7 @@
+#ifndef __MYUTILS_H__
+#define __MYUTILS_H__
+long factorial(int);
+int check_prime(int);
+int checkPalindrome(int number);
+int vsum(int a, ...);
+#endif
