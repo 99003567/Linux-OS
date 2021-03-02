5b723359fdfef5e6a2334f0cfc9b144eed8f8252
99003567 <78537995+99003567@users.noreply.github.com>
2021-03-02 14:41:10 +0530

    Update myutils.c

--- src/myutils.c
+++ src/myutils.c
@@ -1,41 +1,8 @@
-//#include "mystring.h"
 #include "myutils.h"
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
 #include<stdarg.h>
-
-
-
- /*
-int main()
-{
-   int number;
-   long fact = 1;
-   int result;
- 
-   /*printf("Enter a number to calculate it's factorial\n");
-   scanf("%d",&number);
- 
-   printf("%d! = %ld\n", number, factorial(number));
- 
-   return 0;
-    printf("%d\n",factorial(3));
-    if(checkPalindrome(number) == 0){
-        printf("%d is a palindrome number.\n",number);}
-    else if(checkPalindrome(number) == 1){
-        printf("%d is not a palindrome number.\n",number);
-    }
-    result = check_prime(86);
-    if ( result == 1 ){
-      printf("It is prime number.\n");
-    }
-   else if(result == 0){
-      printf("It is not prime number.\n");
-   }
-    printf("%d\n", vsum(3, 2, 3, 4)); 
-    return 0;
-}*/
  
 long factorial(int n)
 {
