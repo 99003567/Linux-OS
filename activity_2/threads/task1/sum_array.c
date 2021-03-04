b23ea144aabdd5ffd5b019d7aa6ab97f03218d4c
99003567 <78537995+99003567@users.noreply.github.com>
2021-03-04 09:41:17 +0530

    Update sum_array.c

--- activity_2/threads/task1/sum_array.c
+++ activity_2/threads/task1/sum_array.c
@@ -27,15 +27,15 @@ int main()
   
     pthread_t threads[MAX_THREAD]; 
   
-    // Creating 4 threads 
+    // Creating 3 threads 
     for (int i = 0; i < MAX_THREAD; i++) 
         pthread_create(&threads[i], NULL, sum_of_arr, (void*)NULL); 
   
-    // joining 4 threads i.e. waiting for all 4 threads to complete 
+    // joining 3 threads i.e. waiting for all 3 threads to complete 
     for (int i = 0; i < MAX_THREAD; i++) 
         pthread_join(threads[i], NULL); 
   
-    // adding sum of all 4 parts 
+    // adding sum of all 3 parts 
     int total = 0; 
     for (int i = 0; i < MAX_THREAD; i++) 
         total += sum[i]; 
