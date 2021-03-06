57c9e604e90cd2d8c32f0441114538a529237928
99003567 <78537995+99003567@users.noreply.github.com>
2021-03-06 13:47:41 +0530

    Delete inc directory

--- inc/bitmask.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BITMASK_H__
-#define __BITMASK_H__
-int set(int n, int k);
-int clear(int n, int k);
-int flip(int n, int k);
-int query(int n, int k);
-#endif
--- inc/myutils.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MYUTILS_H__
-#define __MYUTILS_H__
-long factorial(int);
-int check_prime(int);
-int checkPalindrome(int number);
-int vsum(int a, ...);
-#endif
